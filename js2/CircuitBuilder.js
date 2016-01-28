9a762587988d69f6bd3a1020b75aa1e2d8ed41c8
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2016-01-28 10:56:57 +0100

    Now the buttons work and create objects

--- js2/CircuitBuilder.js
+++ js2/CircuitBuilder.js
@@ -1,18 +1,45 @@
 function CircuitBuilder() {
 
-    var objects = [];
+    var myobjects = [];
 
-    this.addLine = function(data, type) {
-		
+    this.addLine = function(type, linePoints) {
+        create(type, linePoints, false);
     };
-    this.addBorder;
-    this.addCircle;
-	this.addRect;
 
-    function create(typeObject, data) {
-        return {
-            type: type,
-            data: data
+    this.addBorder = function(borderpoints) {
+        create("border", borderpoints, true);
+    };
+
+    this.addCircle = function(center, radius, isStatic, density, friction, restitution) {
+        create("circle", {center: center, radius: radius}, isStatic, density, friction, restitution);
+    };
+
+    this.addRect = function (position, width, heigh, isStatic, density, friction, restitution){
+        create ("rect", {position: position, width:width, heigh: heigh}, isStatic, density, friction, restitution );
+    };
+
+
+    function create(typeObject, data, isStatic, density, friction, restitution) {
+        var obj = {
+            type: typeObject,
+            data: data,
+            isStatic: isStatic,
+            restitution: restitution || null,
+            density: density || null,
+            friction: friction ||  null,
+            color: getRandomColor()
         };
+        myobjects.push(obj);
+        console.log(obj);
     }
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
 }
--- js2/script.js
+++ js2/script.js
@@ -2,16 +2,135 @@ $(document).ready(function() {
 
 
     //Get the canvas and calculate canvas size and max-size
-    var canvas = $('#mycanvas');
-    var ctx = canvas.get(0).getContext('2d');
-    var creationMode = null;
+    var canvas = document.getElementById('mycanvas');
+    var ctx = canvas.getContext('2d');
+    var mode = null;
+    var mousePos = null;
 
-    $(".select-form").click(function(){
+    var linePoints = [],
+        indexline = 0,
+        maxLinePoints = 4;
+
+    var borderpoints = [],
+        indexBorder = 0,
+        maxBorderPoints = 2;
+
+    var circleCenter = null;
+
+    var rectCenter = null;
+
+    var cb = new CircuitBuilder();
+
+
+    $(".select-form").click(function() {
         $(this).addClass("active");
         $(this).siblings().removeClass("active");
-        creationMode = $(this).text();
-        $(".options-pane").empty();
-        
+        mode = $(this).text().toLowerCase();
+        $("." + mode + "-pane").removeClass("hidden").siblings().addClass("hidden");
     });
 
+    canvas.addEventListener('mousemove', function(evt) {
+        mousePos = getMousePos(canvas, evt);
+    }, false);
+
+    // Catch click event to create lines into canvas
+    $('#mycanvas').click(function(event) {
+        switch (mode) {
+            case "rect":
+                rectCenter = mousePos;
+                $("#rectx").text(mousePos.x);
+                $("#recty").text(mousePos.y);
+                break;
+            case "circle":
+                circleCenter = mousePos;
+                $("#centx").text(mousePos.x);
+                $("#centy").text(mousePos.y);
+                break;
+            case "border":
+                borderpoints[indexBorder] = mousePos;
+                $("#b" + indexBorder + "x").text(mousePos.x);
+                $("#b" + indexBorder + "y").text(mousePos.y);
+                indexBorder = (indexBorder + 1) % maxBorderPoints;
+                break;
+            case "line":
+                linePoints[indexline] = mousePos;
+                $("#l" + indexline + "x").text(mousePos.x);
+                $("#l" + indexline + "y").text(mousePos.y);
+                indexline = (indexline + 1) % maxLinePoints;
+                break;
+            default:
+
+        }
+    });
+
+    $('#add-object').click(function(event) {
+        if (!mode) return;
+        switch (mode) {
+            case "line":
+            if(linePoints.length ===0) return;
+                if (linePoints.length === 2)
+                    cb.addLine('line', linePoints);
+                else if (linePoints.length > 2) {
+                    cb.addLine('curve', linePoints);
+                }
+                linePoints = [];
+                indexline = 0;
+                $("#l0x").text("");
+                $("#l0y").text("");
+                $("#l1x").text("");
+                $("#l1y").text("");
+                $("#l2x").text("");
+                $("#l2y").text("");
+                $("#l3x").text("");
+                $("#l3y").text("");
+                break;
+            case "border":
+            if(borderpoints.length === 0) return;
+                cb.addBorder(borderpoints);
+                borderpoints = [];
+                indexBorder = 0;
+                $("#b0x").text("");
+                $("#b0y").text("");
+                $("#b1x").text("");
+                $("#b1y").text("");
+                break;
+            case "circle":
+            if(!circleCenter) return;
+                var radius =$("#radius").val();
+                var cStatic = $("#circle-static").prop("checked");
+                var cden = $("#circle-den").val(),
+                    cfric = $("#circle-fric").val(),
+                    cres = $("#circle-res").val();
+                cb.addCircle(circleCenter, radius, cStatic, cden, cfric, cres);
+                circleCenter = null;
+                $("#centx").text("");
+                $("#centy").text("");
+                break;
+            case "rect":
+            if (!rectCenter) return;
+            var rectW =$("#rectW").val();
+            var rectH =$("#rectH").val();
+                var rStatic = $("#rect-static").prop("checked");
+                var rden = $("#rect-den").val(),
+                    rfric = $("#rect-fric").val(),
+                    rres = $("#rect-res").val();
+                cb.addRect(rectCenter, rectW, rectH, rStatic, rden, rfric, rres);
+                rectCenter = null;
+                $("#rectx").text("");
+                $("#recty").text("");
+                break;
+            default:
+
+        }
+    });
 });
+
+
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
